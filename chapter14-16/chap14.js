facac131aba77017edbeba04c6206d724213886b
LaraibZia16 <iamlaraibzia@gmail.com>
2026-02-28 11:08:24 +0500

    Add array declarations and educational qualifications
    This file contains declarations and initializations of various arrays, including student names, strings, numbers, booleans, mixed types, and education qualifications in Pakistan. It also includes a program to display qualifications and a placeholder for storing student scores.

---
+++ chapter14-16/chap14.js
@@ -0,0 +1,32 @@
+// 1. Declare an empty array using JS literal notation to store student names in future.
+let studentNames = [];
+
+// 2. Declare an empty array using JS object notation to store student names in future.
+let studentNamesObj = new Array();
+
+// 3. Declare and initialize a strings array.
+let stringArray = ["Welcome","To","The", "World", "Of", "JavaScript"];
+
+// 4. Declare and initialize a numbers array.
+let numberArray=[0,1, 2, 3, 4, 5, 6, 7, 8, 9]; // decial numbers array
+
+// 5. Declare and initialize a boolean array.
+let booleanArray=[true, false, true, false];
+
+// 6. Declare and initialize a mixed array.
+let mixedArray=["web development", "course","SMIT", "batch", 18, 9,11, true, false];
+
+//7. Declare and Initialize an array and store available education qualifications in Pakistan.Show the listed qualifications in your browser:
+let qualificationArray=["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil", "PHD",];
+let no=1;
+document.writeln("<h1>Qualifications in Pakistan:</h1>");
+document.writeln("<h2>" + no + ". " + qualificationArray[0] + "</h2>");
+document.writeln("<h2>" + ++no + ". " + qualificationArray[1] + "</h2>");
+document.writeln("<h2>" + ++no + ". " + qualificationArray[2] + "</h2>");
+document.writeln("<h2>" + ++no + ". " + qualificationArray[3] + "</h2>");
+document.writeln("<h2>" + ++no + ". " + qualificationArray[4] + "</h2>");
+document.writeln("<h2>" + ++no + ". " + qualificationArray[5] + "</h2>");
+document.writeln("<h2>" + ++no + ". " + qualificationArray[6] + "</h2>");
+document.writeln("<h2>" + ++no + ". " + qualificationArray[7] + "</h2><br>");
+
+// 8. Write a program to store 3 student names in an array.Take another array to store score of these three students. Assume that total marks are 500 for each student, display the scores & percentages of students:
